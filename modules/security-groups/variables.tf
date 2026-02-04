642acdc5d48a5a59960f45da2ee8fa771e21f05e
yseoy0311 <yseoy0311@gmail.com>
2026-02-04 16:50:27 +0900

    feat(modules): 보안그룹 모듈 생성

---
+++ modules/security-groups/variables.tf
@@ -0,0 +1,32 @@
+  variable "name" {
+    type = string
+  }
+
+  variable "vpc_id" {
+    type = string
+  }
+
+  variable "ingress_rules" {
+    type = list(object({
+      from_port       = number
+      to_port         = number
+      protocol        = string
+      cidr_blocks     = optional(list(string), [])
+      security_groups = optional(list(string), [])
+    }))
+    default = []
+  }
+
+  variable "egress_rules" {
+    type = list(object({
+      from_port       = number
+      to_port         = number
+      protocol        = string
+      cidr_blocks     = optional(list(string), [])
+      security_groups = optional(list(string), [])
+    }))
+    # 기본값: 아웃바운드 전체 허용
+    default = [{
+      from_port = 0, to_port = 0, protocol = "-1", cidr_blocks = ["0.0.0.0/0"], security_groups = []
+    }]
+  }
